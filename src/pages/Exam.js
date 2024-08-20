7e3a2be8dc0d287508520a8dec76e1fe1e6b8328
Saunak Shah <saunak.shah@astics.io>
2024-08-21 00:11:43 +0530

    minor changes

--- src/pages/Exam.js
+++ src/pages/Exam.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Input, Button, Space, message } from 'antd';
-import AddCourseForm from '../components/AddCourseForm'; // Make sure the path is correct
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { post, deleteData } from '../global/api';
+import React, { useState, useEffect } from "react";
+import { Table, Input, Button, Space, message } from "antd";
+import AddCourseForm from "../components/AddCourseForm"; // Make sure the path is correct
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { post, deleteData } from "../global/api";
 
 const Exam = () => {
-
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -13,21 +12,45 @@ const Exam = () => {
 
   // Define columns for the table
   const columns = [
-    { title: 'Course Name', dataIndex: 'course_name', key: 'course_name' },
-    { title: 'Marks', dataIndex: 'course_score', key: 'course_score' },
-    { title: 'Passing Marks', dataIndex: 'course_passing_score', key: 'course_passing_score' },
-    { title: 'Exam Duration', dataIndex: 'course_duration_in_hours', key: 'course_duration_in_hours' },
+    { title: "Course Name", dataIndex: "course_name", key: "course_name" },
+    { title: "Marks", dataIndex: "course_score", key: "course_score" },
     {
-      title: 'Action',
-      key: 'action',
+      title: "Passing Marks",
+      dataIndex: "course_passing_score",
+      key: "course_passing_score",
+    },
+    {
+      title: "Exam Duration",
+      dataIndex: "course_duration_in_hours",
+      key: "course_duration_in_hours",
+    },
+    {
+      title: "Action",
+      key: "action",
       render: (text, record) => {
         return (
           <Space>
-            <Button icon={<EditOutlined />} onClick={() => editCourse(record)} />
-            <Button icon={<DeleteOutlined />} onClick={() => deleteCourse(record)} />
+            <Button
+              type="primary"
+              primary={true}
+              icon={<EditOutlined />}
+              onClick={() => editCourse(record)}
+              style={{ marginRight: 20, marginTop: 5 }}
+            >
+              Edit
+            </Button>
+            <Button
+              type="primary"
+              danger
+              icon={<DeleteOutlined />}
+              style={{ marginTop: 5 }}
+              onClick={() => deleteCourse(record)}
+            >
+              Delete
+            </Button>
           </Space>
-        )
-      }
+        );
+      },
     },
   ];
 
@@ -37,16 +60,18 @@ const Exam = () => {
       message.error("No course ID provided for editing.");
       return;
     }
-  
-    setLoading(true);
-    const endpoint = isEdit ? `/api/courses/${course.course_id}` : '/api/courses';
 
-    course.is_active = true
-    course.category = "A"
-    course.course_duration_in_hours = parseInt(course.course_duration_in_hours)
-    course.course_score = parseInt(course.course_score)
-    course.course_passing_score = parseInt(course.course_passing_score)
-    course.course_max_attempts = parseInt(course.course_max_attempts)
+    setLoading(true);
+    const endpoint = isEdit
+      ? `/api/courses/${course.course_id}`
+      : "/api/courses";
+
+    course.is_active = true;
+    course.category = "A";
+    course.course_duration_in_hours = parseInt(course.course_duration_in_hours);
+    course.course_score = parseInt(course.course_score);
+    course.course_passing_score = parseInt(course.course_passing_score);
+    course.course_max_attempts = parseInt(course.course_max_attempts);
 
     const res = await post(endpoint, course);
     if (res.status == 200) {
@@ -57,9 +82,9 @@ const Exam = () => {
       // setData(updatedCourses);
       fetchData();
 
-      message.success(`Course ${isEdit ? 'updated' : 'added'} successfully.`);
+      message.success(`Course ${isEdit ? "updated" : "added"} successfully.`);
     } else {
-      message.error(`Failed to ${isEdit ? 'update' : 'add'} course.`);
+      message.error(`Failed to ${isEdit ? "update" : "add"} course.`);
     }
     setLoading(false);
     setIsModalVisible(false);
@@ -76,10 +101,10 @@ const Exam = () => {
   };
 
   const deleteCourse = async (course) => {
-    console.log("course", course)
-    const endpoint = `/api/courses/${course.course_id}`
+    console.log("course", course);
+    const endpoint = `/api/courses/${course.course_id}`;
     const res = await deleteData(endpoint, course);
-    fetchData()
+    fetchData();
   };
 
   const { Search } = Input;
@@ -91,9 +116,9 @@ const Exam = () => {
       const apiURL = `${apiHost}/api/courses/${limit}/${offset}`;
       const response = await fetch(apiURL, {
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: localStorage.getItem('token') || ''
-        }
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token") || "",
+        },
       });
       if (response.ok) {
         const rawData = await response.json();
@@ -101,30 +126,33 @@ const Exam = () => {
 
         setData(rawData.data.course);
       } else {
-        console.error('Error fetching data:', response.statusText);
+        console.error("Error fetching data:", response.statusText);
       }
     } catch (error) {
-      console.error('Error during API call:', error);
+      console.error("Error during API call:", error);
     } finally {
       setLoading(false);
     }
   };
   useEffect(() => {
-    
-
     fetchData();
   }, []);
 
   return (
     <div id="exam-container">
-          <Button 
-            style={{ width: '150px', height: '40px', backgroundColor: '#f54290', marginBottom:'10px' }} 
-            type="primary" 
-            block 
-            onClick={addCourse}
-          >
-            Add Course
-          </Button>
+      <Button
+        style={{
+          width: "150px",
+          height: "40px",
+          backgroundColor: "#f54290",
+          marginBottom: "10px",
+        }}
+        type="primary"
+        block
+        onClick={addCourse}
+      >
+        Add Course
+      </Button>
       <Table
         dataSource={data}
         columns={columns}
